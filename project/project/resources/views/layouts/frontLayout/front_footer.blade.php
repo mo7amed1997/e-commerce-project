1e27417c9dec9285435a4ce87a6a292d2de7f0e7
mo7amed1997 <mohamed4700602@gmail.com>
2020-04-25 08:50:17 -0700

    project EC v2

---
+++ project/project/resources/views/layouts/frontLayout/front_footer.blade.php
@@ -0,0 +1,37 @@
+<footer class="py-5  " style="background-color: #74bfc2;">
+    <div class="container">
+
+    <div class="row">
+<div class="col-md-4 text-center">
+<div class="contact-widget  wow animated zoomIn animated" data-wow-delay="0.3s">
+    <i class="fa fa-map-marker fa-4x text-white"></i>
+    <h5 class="main-item text-white">Main Office</h5>
+    <p class="text-white">
+       Assiut 
+    </p>
+</div>
+</div>
+<div class="col-md-4 text-center">
+<div class="contact-widget wow animated zoomIn animated" data-wow-delay="0.6s">
+    <i class="fa fa-phone fa-4x text-white"></i>
+    <h5 class="main-item text-white">Call</h5>
+    <p class="text-white">
+    01024461139
+    </p>
+</div>
+</div>
+<div class="col-md-4 text-center">
+<div class="contact-widget  wow animated zoomIn animated" data-wow-delay="0.9s">
+    <i class="fa fa-envelope fa-4x text-white"></i>
+    <h5 class="main-item text-white">Email us</h5>
+    <p class="text-white">
+      info@ASD_EMAIL.net 
+    </p>
+</div>
+</div>
+
+    </div>
+      <p class="m-0 text-center text-white">Copyright &copy; Your Website 2019</p>
+    </div>
+    <!-- /.container -->
+  </footer>
